8f37d7bd97667fa804e9b826c7b9a111230d4f7d
hkichen <hkichen@gmail.com>
2018-04-22 14:14:41 -0400

    letter constructor almost done

---
+++ letter.js
@@ -0,0 +1,12 @@
+function Letter(chara, show) {
+    this.character = character;
+    this.show = false;
+
+    this.characterToShow = function() {
+        if (this.show === true){
+            return this.character
+        }
+    }
+}
+
+//this constructor should take a word 
