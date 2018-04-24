2e6d6bd5494a974cc060db5e6ba543bf034a9849
hkichen <hkichen@gmail.com>
2018-04-24 12:53:31 -0400

    simplifying things, other way was consufsing

--- letter.js
+++ letter.js
@@ -3,18 +3,13 @@
 var Letter = function(secretLetters){
 	this.display = "_"
 	this.secretLetters = secretLetters;
-	this.showsLetter = false;
-
-	this.correctLetterCheck = function(guess){
-		if(this.secretLetters === guess){
-			this.current = this.secretLetters; 
-			return true;
-		}
-			return false;
+	
+	this.displayLetter = function() {
+		return this.display;
 	}
 
-	this.letterDisplay = function(){
-		return this.display;
+	this.changeDisplay = function(){
+		this.display = secretLetters;
 	}
 };
 
---
+++ word.js
@@ -0,0 +1,21 @@
+//require letters
+var Letter = require('./letter.js');
+
+//constructor to get letters, match letters
+var Word = function(secretWord){
+	this.secretWord = secretWord;
+
+	//check if letters match
+	this.matchLetters = function(secretLetter, letters) {
+		var letterMatched = false;
+		for (var i = 0; i < this.secretWord.length; i++) {
+			if(this.secretWord.charAt(i).toLowerCase() === secretletter.toLowerCase()) {
+				letterMatched = true;
+				letters[i].changeDisplay();
+			}
+		}
+	}
+	return letterMatched;
+}	
+
+module.exports = Word;
